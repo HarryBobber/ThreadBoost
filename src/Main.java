9f6a8aa48208858f9a753e314ee112a2811ccdc6
Ram Talla <ramcharantalla@gmail.com>
2023-11-17 12:44:04 -0500

    Added Executor Service

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,20 @@ public class Main implements Callable<Long> {
     int n;
 
     public Main(){
-        s = 100000000;
-        n = 2;
+        s = Integer.MAX_VALUE;
+        n = 10;
         System.out.println("1 Thread: " + count());
         Future[] futures = new Future[n];
-        ExecutorService executorService = Executors.newFixedThreadPool(n);
+        ExecutorService executorService = Executors.newScheduledThreadPool(n);
         for(int i=0; i<n; i++){
-            futures[i] = executorService.submit(this::count);
+            futures[i] = executorService.submit(this::call);
         }
         long sum = 0;
         try{
             for(int i=0; i<n; i++){
                 sum += (long)(futures[i].get());
             }
-            System.out.println(n + " Thread: " + sum);
+            System.out.println(n + " Thread: " + sum/n);
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
@@ -31,9 +31,11 @@ public class Main implements Callable<Long> {
 
     public long count(){
         long start = System.currentTimeMillis();
-        for(int i=0; i<s; i++){
-
+        int i=0;
+        while(i<s){
+            i++;
         }
+        System.out.println(i);
         long end = System.currentTimeMillis();
         return (end-start);
     }
@@ -41,9 +43,11 @@ public class Main implements Callable<Long> {
     @Override
     public Long call() throws Exception {
         long start = System.currentTimeMillis();
-        for(int i=0; i<s/n; i++){
-
+        int i=0;
+        while(i<s/n){
+            i++;
         }
+        System.out.println(i);
         long end = System.currentTimeMillis();
         return (end-start);
     }
@@ -51,5 +55,5 @@ public class Main implements Callable<Long> {
     public static void main(String[] args){
         Main test = new Main();
     }
-    
+
 }
